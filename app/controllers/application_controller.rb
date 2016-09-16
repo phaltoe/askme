c08a2b07a4b5fe36785f775b0b8915ea85f426da
phaltoe <reverse9@live.com>
2016-09-16 17:08:59 -0300

    changed fb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
     if request.referer == sign_in_url
       super
     else
-      request.referer || root_path
+      stored_location_for(resource) || request.referer || root_path
     end
   end
 
