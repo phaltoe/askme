21d3206959fac3319c206e159905c49cbae18617
phaltoe <reverse9@live.com>
2016-09-19 15:08:07 -0300

    removed some asset pipeline issues for heroku

--- app/assets/javascripts/custom.js
+++
@@ -1,8 +0,0 @@
-$('document').ready(function() {
-$("x").click(function(){
-    $(".alert").fadeOut();
-});
-
-// $("x").click(function(){
-//     $("div.alert.alert-warning.fade.in").fadeOut();
-// });
